101b63c9d3eb94e472815af9b713d00f4228fa7c
Debangshu767 <83083521+Debangshu767@users.noreply.github.com>
2023-07-24 18:29:18 +0530

    Time: 12 ms (5.11%), Space: 17 MB (11.01%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    
+    void combi(vector<int> nums,int i,vector<int> temp)
+    {
+        if( i == nums.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
+            
+        
+        combi(nums,i+1, temp);
+        temp.push_back(nums[i]);
+        combi(nums,i+1,temp);
+        
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<int> temp;
+        
+        combi(nums,0,temp);
+        
+        return ans;
+        
+        
+    }
+};

88178d94749c130ddfef66451e28191ed592cf78
Debangshu767 <83083521+Debangshu767@users.noreply.github.com>
2023-07-31 21:19:23 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (14.61%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,30 +1,46 @@
 class Solution {
 public:
     
-    int robre(vector<int> &nums,int i,int ans,vector<int> &dp)
-    {
-        if(i>=nums.size())
-        {
-            return 0;
-        }
+//     int robre(vector<int> &nums,int i,int ans,vector<int> &dp)
+//     {
+//         if(i>=nums.size())
+//         {
+//             return 0;
+//         }
         
-        if(dp[i] != -1)
-            return dp[i];
+//         if(dp[i] != -1)
+//             return dp[i];
         
         
-        int nocurr = robre(nums,i+1,ans,dp);
-        int curr = nums[i] +  robre(nums,i+2,ans,dp);
+//         int nocurr = robre(nums,i+1,ans,dp);
+//         int curr = nums[i] +  robre(nums,i+2,ans,dp);
             
-            return dp[i] =  max(nocurr,curr);
-    }
+//             return dp[i] =  max(nocurr,curr);
+//     }
     
     int rob(vector<int>& nums) {
         
         int ans = 0;
         vector<int> dp(nums.size()+1,-1);
-      return  robre(nums,0,ans,dp);
+      // return  robre(nums,0,ans,dp);
         
+        dp[0] = nums[0];
         
+        int neg = 0;
+        
+        for(int i = 1;i<nums.size();i++)
+        {
+            int take,nontake;
+            if(i == 1)
+                take = nums[i];
+            else
+            take = nums[i] + dp[i-2];
+            nontake = dp[i-1];
+            
+            dp[i] = max(take,nontake);
+        }
+        
+        return dp[nums.size()-1];
         
         
     }

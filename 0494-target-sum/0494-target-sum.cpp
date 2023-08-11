df6705552e2df9cc900afc00fcb614e5ccd0ee9a
Debangshu Batabyal <83083521+Debangshu767@users.noreply.github.com>
2023-08-11 21:53:02 +0530

    Time: 583 ms (26.82%), Space: 89.9 MB (5.05%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     
-    int finds(vector<int> &nums,int i,int target)
+    int finds(vector<int> &nums, int i, int target, unordered_map<string, int> &dp)
     {
         if(i == nums.size())
-            return target == 0 ;
+            return target == 0;
         
-        return finds(nums,i+1,target - nums[i]) + finds(nums,i+1,target + nums[i]);
+        string key = to_string(i) + "_" + to_string(target);
+        if(dp.find(key) != dp.end())
+            return dp[key];
+        
+        int count = 0;
+        count += finds(nums, i+1, target - nums[i], dp);
+        count += finds(nums, i+1, target + nums[i], dp);
+        
+        return dp[key] = count;
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
-        
-       return finds(nums,0,target);
-        
+        unordered_map<string, int> dp;
+        return finds(nums, 0, target, dp);
     }
 };

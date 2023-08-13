c668bfb0807067de3cd7e83a7fc4d66ad6c9de05
Debangshu Batabyal <83083521+Debangshu767@users.noreply.github.com>
2023-08-13 15:04:30 +0530

    Time: 158 ms (24.34%), Space: 50.6 MB (12.65%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,37 +1,19 @@
 class Solution {
-public:
-    
-    bool jump(vector<int>&nums,int i,vector<int> &dp)
-    {
-        if(i == nums.size()-1)
-            return true;
-        
-        
-        if(dp[i] != -1)
-            return dp[i];
-        
-       if(nums[i] != 0)
-       {
-           for(int ind = 1 ;ind <= nums[i];ind++ )
-           {
-               if(i+ind < nums.size())
-               {
-                   if(jump(nums,i+ind,dp)) return dp[i] =  1;
-               else
-                    dp[i] = 0;
-               }
-                
-           }
-       }
-        return dp[i] =  0;
-        
-    }
-    
+    vector<int> memo;
+    public:
     bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,0);
+        dp[0]=true;
         
-        vector<int> dp(nums.size(),-1);
-        
-      return jump(nums,0,dp);
-        
-    }
+        for(int i=1;i<n;i++){
+             for(int j=i-1;j>=0;j--){
+                 if(dp[j] && j+nums[j]>=i){
+                     dp[i]=true;
+                     break;
+                 }       
+             }           
+        }
+        return dp[n-1];
+  }
 };

6fbe9dd26bb3bbadecd119b793616e448b049882
Debangshu Batabyal <83083521+Debangshu767@users.noreply.github.com>
2023-08-20 21:20:15 +0530

    Time: 27 ms (94.82%), Space: 81.6 MB (76.95%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -5,18 +5,26 @@ public:
         vector<int> ans;
         vector<int> occ(A.size()+1,0);
         
-        
+        int count = 0;
         for(int i = 0;i<A.size();i++)
         {
             
             occ[A[i]]++;
+            
+            if(occ[A[i]] == 2)
+                count++;
+            
             occ[B[i]]++;
-            int count = 0;
-            for(int i = 0;i<occ.size();i++)
-            {
-                if(occ[i] == 2)
-                    count++;
-            }
+            
+            // for(int i = 0;i<occ.size();i++)
+            // {
+            //     if(occ[i] == 2)
+            //         count++;
+            // }
+            
+            if(occ[B[i]] == 2)
+                count++;
+            
             ans.push_back(count);
             
         }

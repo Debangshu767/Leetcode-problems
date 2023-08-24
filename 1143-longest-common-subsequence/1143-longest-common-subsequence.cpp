b1ed7c7871eaf917d2817252cee2e96c3906c2a7
Debangshu Batabyal <83083521+Debangshu767@users.noreply.github.com>
2023-08-24 17:04:38 +0530

    Time: 39 ms (83.17%), Space: 24.4 MB (50.18%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -20,12 +20,40 @@ public:
         
     }
     
-    int longestCommonSubsequence(string text1, string text2) {
+    int longestCommonSubsequence(string t1, string t2) {
         
-        int i = text1.size()-1;
-        int j = text2.size()-1;
-        vector<vector<int>>dp (i+1,vector<int>(j+1,-1));
-       return findlargest(text1,text2,i,j,dp);
+        int ind = t1.size();
+        int jnd = t2.size();
+        vector<vector<int>>dp (ind+1,vector<int>(jnd+1,0));
+        
+            for(int j = 0;j<=jnd;j++)
+            {
+                if(t1[0] == t2[j])
+                 dp[0][j] = 0;
+            } 
+        for(int i = 0;i<=ind;i++)
+            {
+                if(t1[i] == t2[0])
+                 dp[i][0] = 0;
+            }
+        for(int i = 1;i<=ind;i++)
+        {
+            for(int j = 1 ;j<=jnd;j++)
+            {
+                 if(t1[i-1] == t2[j-1])
+                    dp[i][j] =  1+dp[i-1][j-1];
+                else
+                {
+                    int take =  dp[i-1][j];
+                    int notake =  dp[i][j-1];
+        
+                 dp[i][j] =  max(take,notake); 
+                }
+                
+            }
+        }
+        return dp[ind][jnd];
+       // return findlargest(text1,text2,i,j,dp);
         
     }
 };

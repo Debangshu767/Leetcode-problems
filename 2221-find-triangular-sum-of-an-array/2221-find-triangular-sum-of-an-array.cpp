3150a919ce215a28d3daa5a2345679c6111b96c7
Debangshu Batabyal <83083521+Debangshu767@users.noreply.github.com>
2023-09-16 12:58:12 +0530

    Time: 197 ms (75.49%), Space: 18.9 MB (50.65%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.cpp
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
     
-    int findsum(vector<int> &nums, int n)
-    {
-        if(n == 0)
-            return nums[0];
+//     int findsum(vector<int> &nums, int n)
+//     {
+//         if(n == 0)
+//             return nums[0];
         
-        for(int i = 0;i<n;i++)
-        {
-            nums[i] += nums[i+1];
-            if(nums[i] > 9)
-                nums[i] = nums[i]%10;
-        }
+//         for(int i = 0;i<n;i++)
+//         {
+//             nums[i] += nums[i+1];
+//             if(nums[i] > 9)
+//                 nums[i] = nums[i]%10;
+//         }
         
-        // for(int i = 0;i<n;i++)
-        // {
-        //     cout<<nums[i]<< " ";
-        // }
-        // cout<<endl;
+//         // for(int i = 0;i<n;i++)
+//         // {
+//         //     cout<<nums[i]<< " ";
+//         // }
+//         // cout<<endl;
         
-        return findsum(nums,n-1);
-    }
+//         return findsum(nums,n-1);
+//     }
     
     
     int triangularSum(vector<int>& nums) {
         
-        int n = nums.size()-1;
-       return findsum(nums,n);
+       //  int n = nums.size()-1;
+       // return findsum(nums,n);
+
+     int n(size(nums));
+        while (n > 1) {
+            for (int i=0; i<n-1; i++)
+                nums[i] = (nums[i] + nums[i+1]) % 10;
+            n--;
+        }
+        return nums[0];
         
     }
 };

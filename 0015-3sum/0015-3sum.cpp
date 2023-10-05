47c72d519a1bea6b0975995210af6dbf255617e5
Debangshu Batabyal <83083521+Debangshu767@users.noreply.github.com>
2023-10-05 17:05:20 +0530

    Time: 220 ms (39.91%), Space: 26.4 MB (45.38%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,9 +3,12 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         
         sort(nums.begin(),nums.end());
-        set<vector<int>> sans;
+        //set<vector<int>> sans;
+        vector<vector<int>> ans;
         for(int i = 0;i<nums.size();i++)
         {
+            if( i != 0 && nums[i] == nums[i-1])
+                continue;
             int j = i+1;
             int k = nums.size()-1;
             
@@ -14,8 +17,16 @@ public:
                 if(nums[i]+nums[j]+nums[k] == 0)
                 {
                     vector<int> temp = {nums[i],nums[j],nums[k]};
-                    sans.insert(temp);
+                    //sans.insert(temp);
+                    ans.push_back(temp);
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
                     j++;
+                    
+                    while (j < k && nums[k - 1] == nums[k]) {
+                        k--;
+                    }
                     k--;
                 }
                 else if(nums[i]+nums[j]+nums[k] <0)
@@ -25,7 +36,7 @@ public:
             }
         }
         
-        vector<vector<int>> ans(sans.begin(),sans.end());
+        //vector<vector<int>> ans(sans.begin(),sans.end());
         
         return ans;
         
